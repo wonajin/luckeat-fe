c5aaeadcbd59e64b8726162fcf663d61428c5ff5
won a-jin <ajinwon1@gmail.com>
2025-03-12 15:35:47 +0900

    Update commitlint.config.cjs

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,36 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [
+      2,
+      'always',
+      [
+        'sentence-case',
+        'start-case',
+        'pascal-case',
+        'upper-case',
+        'lower-case',
+      ],
+    ],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'content',
+        'docs',
+        'feat',
+        'fix',
+        'refactor',
+        'style',
+        'test',
+        'deploy',
+      ],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-min-length': [2, 'always', 5],
+    'header-max-length': [2, 'always', 72],
+  },
+}
